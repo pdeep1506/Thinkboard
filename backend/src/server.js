ce341df2d8bc2211da3f59454b9c850c911add6e
pdeep1506 <pdeep1506@gmail.com>
2026-05-28 08:22:41 +0530

    created schema

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,16 @@ import express from "express";
 import notesRoute from './routes/notesRoute.js'
 import { connectDB } from "./config/db.js";
 import dotenv from 'dotenv';
-const app = express();
 
 dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5001;
+
 connectDB();
 app.use('/api/notes', notesRoute);
 
 
 
-app.listen(5001, ()=>{
+app.listen(PORT, ()=>{
     console.log("server started on PORT:5001")
 })
