d6229ecd2001f8666a7a731dd5c002f30115cf1f
pdeep1506 <pdeep1506@gmail.com>
2026-05-30 05:54:35 +0530

    navbar componenet

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../components/Navbar'
+import RateLimitedUI from '../components/RateLimitedUI'
 
 const HomePage = () => {
+   const [isRateLimited,seIsRateLimited ] = useState(false)
   return (
-    <div>HomePage</div>
+    <div className='min-h-screen'>
+
+      <Navbar></Navbar>
+      {isRateLimited && <RateLimitedUI></RateLimitedUI>}
+
+    </div>
   )
 }
 
