69b9dcb04fdd4ddb03dd3ba61ade25df25816169
pdeep1506 <pdeep1506@gmail.com>
2026-05-30 06:31:40 +0530

    .

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -8,7 +8,7 @@ dotenv.config();
 //? Ratelimited that allow 100 requests per 60 seconds
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(10, "60 s")
+    limiter: Ratelimit.slidingWindow(100, "60 s")
 })
 
 export default ratelimit
