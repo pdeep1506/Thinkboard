ad3cffb99f73dedf90172938044fa55949ce2cfc
pdeep1506 <pdeep1506@gmail.com>
2026-06-01 11:32:40 +0530

    .

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,8 @@ import RateLimitedUI from '../components/RateLimitedUI'
 import axios from 'axios';
 import toast from 'react-hot-toast';
 import NoteCard from '../components/NoteCard';
+import NotesNotFound from '../components/NotesNotFound';
+
 const HomePage = () => {
    const [isRateLimited,setIsRateLimited ] = useState(false)
    const [notes,setNotes] = useState([]);;
@@ -45,6 +47,7 @@ const HomePage = () => {
           </div>
         }
 
+        {notes.length === 0 && !isRateLimited && <NotesNotFound></NotesNotFound>}
         {notes.length > 0 && !isRateLimited &&(
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>{
 
