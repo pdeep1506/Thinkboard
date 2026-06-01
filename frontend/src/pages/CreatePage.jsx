6a51e945d039dd82a36f6f6906ad050a534001c3
pdeep1506 <pdeep1506@gmail.com>
2026-06-01 10:16:53 +0530

    createPage funcationalty implemented

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,11 +1,52 @@
 import React, {useState} from 'react'
-
+import {Link, useNavigate} from 'react-router';
+import {ArrowLeftIcon} from 'lucide-react';
+import toast from 'react-hot-toast';
+import axios from 'axios';
 const CreatePage = () => {
+  const navigate = useNavigate();
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = ()=>{
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log(title)
+    console.log(content)
+
+    if(!title.trim() || !content.trim()){
+      toast.error("All fields are required")
+      return;
+    }
+
+    setLoading(true)
+    try{
+      const saveNote = async()=>{
+
+        const createNote = await axios.post('http://localhost:5001/api/notes/', {
+          title, content
+        })
+        if(createNote.status === 201){
+           console.log('Note created successfully');
+           toast.success("Note created successfully!")
+           navigate("/");
+        }
+      }
+      saveNote();
+    }
+    catch(error){
+      if(error.response.status === 429){
+          toast.error("Slow down! you are creating notes too fast",{
+            duration:4000,
+            
+          });
+      }else{
+        toast.error("Failed to create Note")
+      }
+    }
+    finally{
+      setLoading(false)
+    }
 
   }
   
@@ -13,11 +54,54 @@ const CreatePage = () => {
     <div className='min-h-screen bg-base-200'>
       <div className='container mx-auto px-4 py-8'>
           <div className='max-w-2xl mx-auto'>
-            
+            <Link to={'/'} className='btn btn-ghost mb-6'>
+              <ArrowLeftIcon className='size-5'>
+              </ArrowLeftIcon>
+                Back to Notes
+            </Link>
+            <div className='card bg-base-100'>
+              <div className='card-body'>
+                <h2 className='card-title text-2xl mb-4'>Create a new note</h2>
+                <form onSubmit={handleSubmit}>
+                  <div className='form-control mb-4'>
+                    <label  className='label'>
+                        <span className='label-text'>Title</span>
+                    </label>
+                    <input type='text'
+                    placeholder='Note title'
+                    className='input input-bordered'
+                    value={title}
+                    onChange={((e)=>{ setTitle(e.target.value)})}
+                    ></input>
+                  </div>
+                  {/* Content */}
+                  <div className='form-control mb-4'>
+                    <label  className='label'>
+                        <span className='label-text'>Content</span>
+                    </label>
+                    <textarea type='text'
+                    placeholder='Note Content'
+                    className='input input-bordered'
+                    value={content}
+                    onChange={((e)=>{ setContent(e.target.value)})}
+                    ></textarea>
+                  </div>
+
+                  {/* Action button */}
+                  <div className='card-actions justify-end'>
+                    <button type='submit' onClick={(e)=>{handleSubmit(e)}}  className='btn btn-primary' disabled={loading}>
+                      {loading ? "Creating..." :"Create Note"}
+                    </button>
+
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
       </div>
     </div>
   )
+
 }
 
 export default CreatePage
