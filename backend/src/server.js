0629533ad55643b13cb4422c68daf42605677896
pdeep1506 <pdeep1506@gmail.com>
2026-06-04 10:14:55 +0530

    finsihing

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,20 @@ import dotenv from 'dotenv';
 import notesRoute from './routes/notesRoute.js'
 import { connectDB } from "./config/db.js";
 import { rateLimiter } from "./middleware/rateLimiter.js";
+import path from 'path';
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
-app.use(cors({
-    origin:'http://localhost:5173'
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
 }
-))
 // Middleware
 app.use(express.json())
 
@@ -25,7 +31,13 @@ app.use(rateLimiter)
 app.use('/api/notes', notesRoute);
 
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(()=>{
 app.listen(PORT, ()=>{
