ef17a5b93aae15be37809691c11a7c6c4859bf34
pdeep1506 <pdeep1506@gmail.com>
2026-06-04 12:36:35 +0530

    baseURL change

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -37,7 +37,7 @@ const NoteDetailPage = () => {
     setSaving(true);
 
     try{
-      const updateNotes = await axios.put(`https://thinkboard-backend.onrender.com/api/notes/${id}`, note);
+      const updateNotes = await axios.put(`https://thinkboard-pdeep1506.onrender.com/api/notes/${id}`, note);
       toast.success("Note updated successfully");
       navigate("/");
     }
