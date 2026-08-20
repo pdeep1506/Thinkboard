22e7c58c6f6c0a1cfc831e45d69e1d4ad8bcac27
pdeep1506 <pdeep1506@gmail.com>
2026-08-20 18:13:07 +0530

    add one endpoint

--- backend/src/routes/notesRoute.js
+++ backend/src/routes/notesRoute.js
@@ -2,6 +2,9 @@ import express from 'express';
 import { createNotes, updateNotes, deleteNotes, getNotes, getNoteById } from '../controllers/notesController.js';
 const router = express.Router();
 
+router.get('/test', async()=>{
+    return res.status(200).json({success: true ,Message:"Testing Successfull"});
+})
 router.get('/', getNotes)
 router.get('/:id', getNoteById)
 router.post('/', createNotes)
