6b9ce53e1cd94d52c2ca60a253400df7c02841ba
pdeep1506 <pdeep1506@gmail.com>
2026-08-20 18:20:55 +0530

    Connect Express API to Netlify function

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -1,23 +1,20 @@
-// import serverless from "serverless-http";
-// import app from "../../src/server.js";
-// import { connectDB } from "../../src/config/db.js"
+import serverless from "serverless-http";
+import app from "../../src/server.js";
+import { connectDB } from "../../src/config/db.js"
 
-// const handler = serverless(async (req, res) => {
-//   await connectDB();
-//   return app(req, res);
-// });
+let dbConnected = false;
 
-// export { handler };
+const handler = serverless(async (req, res) => {
+  if (!dbConnected) {
+    await connectDB();
+    dbConnected = true;
+  }
+});
 
-export const handler = async (event) => {
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      message: "ThinkBoard backend is working!",
-      path: event.path,
-    }),
-  };
+export const handler = async (event, context) => {
+  await connectToDatabase();
+
+  const serverlessHandler = serverless(app);
+
+  return serverlessHandler(event, context);
 };
