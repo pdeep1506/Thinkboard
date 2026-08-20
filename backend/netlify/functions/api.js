644943027959422a088d2a9b2d1de942bc80c50f
pdeep1506 <pdeep1506@gmail.com>
2026-08-20 18:17:39 +0530

    Configure Netlify function

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -1,10 +1,23 @@
-import serverless from "serverless-http";
-import app from "../../src/server.js";
-import { connectDB } from "../../src/config/db.js"
+// import serverless from "serverless-http";
+// import app from "../../src/server.js";
+// import { connectDB } from "../../src/config/db.js"
 
-const handler = serverless(async (req, res) => {
-  await connectDB();
-  return app(req, res);
-});
+// const handler = serverless(async (req, res) => {
+//   await connectDB();
+//   return app(req, res);
+// });
 
-export { handler };
+// export { handler };
+
+export const handler = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      message: "ThinkBoard backend is working!",
+      path: event.path,
+    }),
+  };
+};
